036c4271421b2b482976101d620bbdc87aaf37a0
Yusif Shikhseyidov <yusif1580@gmail.com>
2023-12-22 01:42:05 +0400

    I fixed some bugs related to responsiveness

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 import classes from "./News.module.css";
+import {TbArrowBadgeRightFilled} from "react-icons/tb"
 
 export default function News() {
   return (
     <section className={classes.sectionNews}>
-      <Link>
+      <Link to="/">
         <div>
           More News
         </div>
+        <TbArrowBadgeRightFilled />
       </Link>
     </section>
   );
