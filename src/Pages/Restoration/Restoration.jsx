5aa0c27dd25b862dfe09421ace46255896705ffe
Yusif Shikhseyidov <yusif1580@gmail.com>
2023-12-23 16:19:29 +0400

    I amde website fully responsive

--- src/Pages/Restoration/Restoration.jsx
+++ src/Pages/Restoration/Restoration.jsx
@@ -161,9 +161,13 @@ export default function Restoration() {
           <div className={classes.gridDesignElements}>
             <Fade direction="up" triggerOnce={true}>
               <div className={classes.designPhotoAndInfoContainerGrid}>
-                <img src="https://cdn.antonovich-design.ae/uploads/page/2021/11/antonovich-design-thumb2021UAZ2GSAS8KVY.jpeg" alt="exterior-design-example" />
-                <h3>Exterior Design</h3>
-                <Link>Explore Service</Link>
+                
+                  <img src="https://cdn.antonovich-design.ae/uploads/page/2021/11/antonovich-design-thumb2021UAZ2GSAS8KVY.jpeg" alt="exterior-design-example" />
+                
+                
+                  <h3>Exterior Design</h3>
+                  <Link>Explore Service</Link>
+                
               </div>
               <div className={classes.designPhotoAndInfoContainerGrid}>
                 <img src="https://cdn.antonovich-design.ae/uploads/page/2021/11/antonovich-design-thumb2021MoIJbTN4pBur.jpg" alt="exterior-design-example" />
@@ -171,9 +175,13 @@ export default function Restoration() {
                 <Link>Explore Service</Link>
               </div>
               <div className={classes.designPhotoAndInfoContainerGrid}>
-                <img src="https://cdn.antonovich-design.ae/uploads/page/2022/5/antonovich-design-thumb2022FhHT60nkPIb2.jpg" alt="exterior-design-example" />
-                <h3>Exterior Design</h3>
-                <Link>Explore Service</Link>
+                
+                  <img src="https://cdn.antonovich-design.ae/uploads/page/2022/5/antonovich-design-thumb2022FhHT60nkPIb2.jpg" alt="exterior-design-example" />
+                
+                
+                  <h3>Exterior Design</h3>
+                  <Link>Explore Service</Link>
+                
               </div>
             </Fade>
             <Fade direction="up" triggerOnce={true}>
